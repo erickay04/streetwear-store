7d8196e57096cf25cff6fdfc941b76a534e4c475
Kotun Babajide <kotunbabajide@gmail.com>
2026-07-01 13:36:41 +0100

    Configure vite for GitHub pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import { FaShoppingCart, FaUser } from "react-icons/fa"
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <header>
       <h1>DRIPSTORE</h1>
 
       <nav>
-        <ul>
+        <ul className={menuOpen ? "nav-links active" : "nav-links"}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/products">Shop</Link></li>
           <li><Link to="/about">About Us</Link></li>
@@ -19,6 +21,14 @@ const Header = () => {
         <Link to="/cart"><FaShoppingCart className="icon"/></Link>
         <Link to="/create"><FaUser className="icon"/></Link>
       </div>
+      <div className="burger"
+        onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+
     </header>
     
   );
